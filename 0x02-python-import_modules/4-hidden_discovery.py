66fddbbeb61a6b7a63c57e9037988ca9825b793f
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-11 22:36:17 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 
-if name in dir(hidden_4):
-    if not name.startswith("__"):
-        print(name)
+if __name__ == "__main__":
+    if name in dir(hidden_4).sort():
+        if not name.startswith("__"):
+            print(name)
