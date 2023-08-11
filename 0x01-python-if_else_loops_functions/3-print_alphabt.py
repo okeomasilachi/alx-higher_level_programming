0965565648b69769b4ae03515e444daa448da256
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-11 10:04:22 +0100

    7-islower.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for pos in range(97, 123):
-    if pos == 101:
-        continue
-    if pos == 106:
+    if pos == 101 or pos == 106:
         continue
     print("{0}".format(chr(pos)), end="")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    val = ord(c)
+    if val >= 97 and val <= 123:
+        return True
+    else:
+        return False
