eabe7fd6c639922c20392ac9517eebea2c76a093
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-11 10:49:34 +0100

    ./8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(name):
+    i = 0
+    while i < len(name):
+        if ord(name[i]) >= 97 and ord(name[i]) <= 123:
+            print("{}".format(chr(65 + (ord(name[i]) - 97))),
+                  end="\n" if i == (len(name) - 1) else "")
+        else:
+            print(name[i], end="\n" if i == (len(name) - 1) else "")
+        i += 1
