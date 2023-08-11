942b1b34ff4d50dee32a53f285adf4f62dd753de
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-11 21:28:08 +0100

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
-val = add(a, b)
-print("{} + {} = {}".format(a, b, val))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    val = add.add(a, b)
+    print("{} + {} = {}".format(a, b, val))
