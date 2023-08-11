049861b7e7cd1f6381e58acdbc8b0a7ad4025ec6
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-11 13:05:07 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def print_two_digits(i, j):
     if i < j:
-        print("{}{}, ".format(i, j), end="")
+        print("{}{}".format(i, j), end="\n" if i == 8 and j == 9 else ", ")
 
 
 for i in range(10):
     for j in range(i + 1, 10):
         print_two_digits(i, j)
 
-print()
