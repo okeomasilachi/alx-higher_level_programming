80e3cb2eaf31496f66f71d7c63efabd6abe9884e
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-11 22:17:31 +0100

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    result = 0
+    for i in range(1, (len(argv))):
+        result = result + int(argv[i])
+
+    print(f"{result}")
