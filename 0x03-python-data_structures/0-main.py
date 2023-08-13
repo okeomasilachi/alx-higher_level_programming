8f793f0bc2e47afde0774519d242d013fc3f9300
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-13 20:20:28 +0100

    0-print_list_integer.py

--- 0x03-python-data_structures/0-main.py
+++ 0x03-python-data_structures/0-main.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+print_list_integer = __import__('0-print_list_integer').print_list_integer
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+my_list = [1, "2", 3, 4, 5]
+print_list_integer(my_list)
--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for val in my_list:
-        if isinstance(val, int):
+        if type(val) is int or type(val) is float:
             print("{:d}".format(val))
