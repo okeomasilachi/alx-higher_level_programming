f35d44fe4d68b4baf69a15131dabaa05c875d0b6
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-13 18:32:41 +0100

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".format(num))
+    for val in my_list:
+        if isinstance(val, int):
+            print("{}".format(val))
