ba2595bfb91e372298dff17930e7698097e1fcb1
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-13 19:18:16 +0100

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     i = (len(my_list) - 1)
+    t = int, float
     while i > -1:
-        if type(my_list[i]) is int or type(my_list[i]) is float:
+        if isinstance(my_list[i], t):
             print("{}".format(my_list[i]))
         i -= 1
