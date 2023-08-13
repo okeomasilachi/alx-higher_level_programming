a763000b0cc2c18a4007cb1e7201ed1fe69e2a84
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-13 19:30:41 +0100

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    for i in my_list:
+        new_list.append(i)
+    if idx < 0:
+        return new_list
+    elif idx > (len(my_list) - 1):
+        return new_list
+    new_list[idx] = element
+    return new_list
