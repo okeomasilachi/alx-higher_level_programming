aad4c42d0230b06f2ccedd36d333c37f33b69912
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-14 20:49:35 +0100

    12-switch.py

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <python3.4m/Python.h>
+#include <python3.4m/object.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: struct of type PyObject
+ *
+ * Return: void
+*/
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size = PyList_Size(p), i;
+	PyObject *item;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
