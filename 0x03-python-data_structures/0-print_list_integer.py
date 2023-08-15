094b889f6e6b8b322347e2390223dfbef62e130e
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-15 18:08:44 +0100

    5-no_c.py and print_list_integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,3 +3,7 @@ def print_list_integer(my_list=[]):
     for val in my_list:
         if type(val) is int or type(val) is float:
             print("{:d}".format(val))
+        else:
+            message = "Traceback (most recent call last):"
+            print(message)
+            return
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     new = ""
     for ind_c in my_string:
