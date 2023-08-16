0ddc0cd6feb1836ab9ddc6bba7acb9526b089db6
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-16 23:36:59 +0100

    debug only_diff_elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return set()
+    if not set_1:
+        return set_2
+    elif not set_2:
+        return set_1
     else:
         return set_1 ^ set_2
