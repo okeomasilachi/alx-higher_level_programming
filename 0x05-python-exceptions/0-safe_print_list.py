26d9a4ab9d7328fd50592f45e98a276f40667711
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-28 15:22:50 +0100

    creat file and folder for the python exception projects

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    def safe_print_list(my_list=[], x=0):
-        ct = 0
-        try:
-            for i in range(x):
-                print(my_list[i], end="")
-                ct += 1
-        except IndexError as er:
-            print("\nError:", er, end="")
-        finally:
-            print()
-            return ct
+
+def safe_print_list(my_list=[], x=0):
+    if not my_list or not x or x == 0:
+        return 0
+    ct = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            ct += 1
+    except IndexError as er:
+        print("\nError:", er, end="")
+    finally:
+        print()
+        return ct
