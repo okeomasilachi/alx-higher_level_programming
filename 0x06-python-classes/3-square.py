21778c24e709c210f7d9de5328d973593f0ca02a
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-29 21:09:35 +0100

    3-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+""" defines a square by: (based on 2-square.py) """
+
+
+class Square:
+    """A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square's sides.
+
+    Raises:
+        TypeError: If the size is not an integer.
+        ValueError: If the size is less than 0.
+    """
+    def __init__(self, size=0):
+        """Initialize a square with a given size.
+
+        Args:
+            size (int, optional): The size of the square's sides. Default is 0.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size  # The size of the square is private.
+
+    def area(self):
+        """Get the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return (self.__size * self.__size)
--- 0x06-python-classes/print.py
+++ 0x06-python-classes/print.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-my_module = "2-square"
+my_module = "3-square"
 my_function = "__init__"
 MyClass = "Square"
 
 print(__import__(my_module).__doc__)
 print(__import__(my_module).Square.__doc__)
-print(__import__(my_module).__init__.__doc__)
 print(__import__(my_module).Square.__init__.__doc__)
+print(__import__(my_module).Square.area.__doc__)
