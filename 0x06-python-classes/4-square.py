9890db22a1953a05b1be76ba606b08633e17032c
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-29 22:01:53 +0100

    property def size(self): to retrieve it property setter def size(self, value)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -30,10 +30,35 @@ class Square:
         else:
             self.__size = size  # The size of the square is private.
 
+    # Define a property for the size attribute.
+    @property
+    def size(self):
+        """Get the square's size."""
+        return (self.__size)
+
+    # Define a setter method for the size attribute.
+    @size.setter
+    def size(self, value):
+        """Set the square's size.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value  # The size of the square is private.
+
     def area(self):
         """Get the area of the square.
 
         Returns:
             int: The area of the square.
         """
-        return (self.__size * self.__size)
+        return self.__size * self.__size
