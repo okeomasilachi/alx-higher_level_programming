5d3adb4209902d29845e266293b1395f74a526e5
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-29 04:07:18 +0100

    debug safe_print_integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as v:
+    except ValueError:
+        return False
+    except TypeError:
         return False
