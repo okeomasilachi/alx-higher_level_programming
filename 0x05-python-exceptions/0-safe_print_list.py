3afee7b87f56689768b7ba30453c08293ab034b8
Okeomasilachi <okeomasilachi@gmail.com>
2023-08-29 08:23:45 +0100

    debug code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,15 @@
 
 
 def safe_print_list(my_list=[], x=0):
+    """safe_print_list
+
+    Args:
+        my_list (list, optional): [The list to print]. Defaults to [].
+        x (int, optional): [number of element to print from the list]. Defaults to 0.
+
+    Returns:
+        [int]: [Actual number of items printed]
+    """
     ct = 0
     for i in range(x):
         try:
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,11 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             ctn += 1
-        except IndexError as er:
-            print(er, end="")
-        else:
-            continue
-        finally:
-            continue
-    print()
+        except IndexError:
+            print("Traceback (most recent call last):")
+            break
     return ctn
