1e0e75f064c4ca62166cde283a3325043b4b1bf1
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-04 13:17:21 +0100

    Write an empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Module that defines all possible methods for tha class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    class Rectangle defines a rectangle class
+    """
+    pass
