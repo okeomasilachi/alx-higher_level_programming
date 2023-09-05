82a1865bbcb1c9fd709ebec709e8efdbece4eecd
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-05 22:05:52 +0100

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,39 @@
 #!/usr/bin/python3
+
+"""
+class LockedClass with no class or object attribute,
+that prevents the user from dynamically creating new
+instance attributes, except if the new instance attribute
+is called first_name.
+"""
+
+
 class LockedClass:
+    """
+    LockedClass.
+
+    A class that prevents the user from dynamically
+    creating new instance attributes, except if the new
+    instance attribute is called first_name.
+
+    Attributes:
+        __slots__: A special attribute that lists the names
+                    of the allowedinstance attributes.
+                    In this case, we have set __slots__ to an
+                    empty list, which means that no new instance
+                    attributes are allowed.
+
+        __setattr__: A special method that is called whenever
+                    an attribute is assigned to an object. In
+                    this case, we have overridden the __setattr__
+                    method to check if the attribute name is "first_name".
+                    If it is, then we allow the attribute to be assigned.
+                    Otherwise, we raise an `AttributeError` exception.
+    """
     def __setattr__(self, nm, value):
+        """
+        The function that does and handle the logic
+        """
         if nm == "first_name":
             super().__setattr__(nm, value)
         else:
