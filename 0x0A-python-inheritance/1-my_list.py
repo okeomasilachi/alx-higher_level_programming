089a5a67a20c81588dcff6e7c3a9c897d2ab5067
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-11 20:31:03 +0100

    Mylist classof the baseclass list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Module that host the class MyList(list).
+"""
+
+
+class MyList(list):
+    """
+    The class MyList is a subclass of the built-in list class in Python
+    """
+    def print_sorted(self):
+        """
+        The function `print_sorted` takes a list
+        creates a copy of it, sorts the copy in ascending
+        order, and then prints the sorted copy.
+        """
+        ok = self.copy()
+        ok.sort()
+        print(ok)
