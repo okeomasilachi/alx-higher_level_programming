33f6198b377e4e46fb34eae4556954c9632a9226
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-11 22:00:53 +0100

    class BaseGeometry:

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""model for the geometry base class"""
+
+
+class BaseGeometry:
+    """
+    The BaseGeometry class is a base class for
+     geometry-related classes.
+    """
+    pass
