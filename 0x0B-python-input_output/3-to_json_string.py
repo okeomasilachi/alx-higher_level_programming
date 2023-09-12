5a22fcdceb3254b796b80b7c62f9ab500192a419
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-12 22:41:54 +0100

    def to_json_string(my_obj):

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+
+""" holds the to_jason_string function """
+
+
+def to_json_string(my_obj):
+    """
+    converts a Python object into a JSON string.
+
+    Args:
+      my_obj: the object that you want to convert to a JSON string.
+      It can be any valid Python object such as a dictionary,
+      list, string, number, boolean, or None.
+
+    Returns:
+      a JSON string representation of the input object.
+    """
+    return json.dumps(my_obj)
