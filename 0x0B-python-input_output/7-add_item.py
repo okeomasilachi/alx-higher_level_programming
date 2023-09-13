ca4cce8dedd64bef9e08ba1d5494d4d5325607ad
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-13 14:39:28 +0100

    7-add_item.py script that adds all arguments to a Python list. save them to a file.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
+"""
+Script that adds all arguments to a Python list.
+and then save them to a file named add_item.json.
+If the file doesn’t exist, it should be created
+"""
+
+
 import sys
+
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 args = sys.argv[1:]
+try:
+    var = load_from_json_file("add_item.json")
+except Exception:
+    var = []
+finally:
+    for item in args:
+        var.append(item)
 
-print(args)
+    save_to_json_file(var, "add_item.json")
