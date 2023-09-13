5b683af3e09bf7f5bf4cb4429f13939eb2e562df
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-13 18:31:54 +0100

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """
+    Generate the first n rows of Pascal's triangle.
+
+    Args:
+        n: The number of rows to generate.
+
+    Returns:
+        A list of lists, where each inner list is a row of the triangle.
+    """
+    ok = []
+    if n <= 0:
+        return ok
+    tri = [[1]]
+    for i in range(1, n):
+        rw = [1]
+        for j in range(1, i):
+            rw.append(tri[i - 1][j - 1] + tri[i - 1][j])
+        rw.append(1)
+        tri.append(rw)
+    return tri
