593171fa5416391dd62501470285cabebda7af1b
Okeomasilachi <okeomasilachi@gmail.com>
2023-10-19 19:58:27 +0100

    utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,12 @@
+-- converts hbtn_0c_0 database to UTF8
+-- (utf8mb4, collate utf8mb4_unicode_ci)
+-- in your MySQL server.
+ALTER DATABASE oki CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+MODIFY COLUMN name VARCHAR(256)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
