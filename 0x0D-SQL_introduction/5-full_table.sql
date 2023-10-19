98abf72a79979c21e7edc3ea0b92a83889bb4872
Okeomasilachi <okeomasilachi@gmail.com>
2023-10-19 13:57:08 +0100

    displays description of the table first table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,6 @@
 -- script that prints the full description of the
 -- table first_table from the database hbtn_0c_0
 -- in your MySQL server.
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM information_schema.COLUMNS
+SELECT * FROM information_schema.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0'
 AND TABLE_NAME = 'first_table';
