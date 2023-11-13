f20bbf4c7226150e54c76ece28018226a9c43241
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-13 22:18:02 +0100

    Write a function that executes x times a function.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+
+exports.callMeMoby = function (X, theFunction) {
+  for (let i = 0; i < X; i++) {
+    theFunction();
+  }
+};
--- 0x12-javascript-warm_up/go.js
+++ 0x12-javascript-warm_up/go.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-myVar = 89;
+/* myVar = 89;
 require('./100-let_me_const');
 console.log(myVar);
+*/
+
+const callMeMoby = require('./101-call_me_moby').callMeMoby;
+callMeMoby(3, function () {
+  console.log('C is fun');
+});
