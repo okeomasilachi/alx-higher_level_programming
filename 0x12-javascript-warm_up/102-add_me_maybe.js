de1a5fa93ef8926674b1aa18f8ff628fffc6f7b3
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-13 22:22:28 +0100

    Write a function that increments and calls a function.

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+
+exports.addMeMaybe = function (num, theFunction) {
+  theFunction(num + 1);
+};
--- 0x12-javascript-warm_up/go.js
+++ 0x12-javascript-warm_up/go.js
@@ -4,7 +4,14 @@ require('./100-let_me_const');
 console.log(myVar);
 */
 
-const callMeMoby = require('./101-call_me_moby').callMeMoby;
+/* const callMeMoby = require('./101-call_me_moby').callMeMoby;
 callMeMoby(3, function () {
   console.log('C is fun');
+}); */
+
+const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
+
+addMeMaybe(4, function (nb) {
+  console.log('New value: ' + nb);
 });
+
