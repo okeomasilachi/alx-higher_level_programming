66e1489430822f8f51bcebc05e880ef8e916f6ed
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-13 20:56:27 +0100

    Write a script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const [args] = process.argv.slice(2);
+
+const num = parseInt(args);
+
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) { console.log('X'.repeat(num)); }
+} else {
+  console.log('Missing size');
+}
