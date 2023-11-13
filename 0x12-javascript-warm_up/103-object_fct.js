82baf6d0af802429ef222cc4945e27172163a707
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-13 22:29:01 +0100

    Update this script by adding a new function incr that increments the integer value.

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function incr () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
--- 0x12-javascript-warm_up/go.js
+++ 0x12-javascript-warm_up/go.js
@@ -14,4 +14,3 @@ const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
 addMeMaybe(4, function (nb) {
   console.log('New value: ' + nb);
 });
-
