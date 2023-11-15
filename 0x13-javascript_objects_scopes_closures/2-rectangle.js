3c1c64e700ec7b7a1923525c80fb24e8b8e8b3e3
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-15 21:33:33 +0100

    aWrite a class Rectangle that defines a rectangle:
    You must use the class notation for defining your class
    The constructor must take 2 arguments w and h
    Initialize the instance attribute width with the value of w
    Initialize the instance attribute height with the value of h
    If w or h is equal to 0 or not a positive integer, create an empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+}
+
+module.exports = Rectangle;
