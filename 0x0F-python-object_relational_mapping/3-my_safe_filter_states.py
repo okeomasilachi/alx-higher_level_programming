45e560109395dcba8d0811ef5434506d251c6685
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-17 10:36:49 +0100

    debug code

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+""" The module lists all states from the database hbtn_0e_0_usa """
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=args[0],
+        passwd=args[1],
+        db=args[2]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = \
+                   %s ORDER BY id ASC", (args[3],))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+
+""" The module lists all states from the database hbtn_0e_0_usa """
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=args[0],
+        passwd=args[1],
+        db=args[2]
+    )
+    cursor = db.cursor()
+    quary = """
+    SELECT GROUP_CONCAT(name ORDER BY id ASC SEPARATOR ', ') 
+    FROM cities 
+    WHERE state_id = (SELECT id FROM states WHERE name = %s)
+    """
+    cursor.execute(quary, (args[3],))
+    states = cursor.fetchall()
+    for state in states:
+        for i in state:
+            print(i)
+    cursor.close()
+    db.close()
