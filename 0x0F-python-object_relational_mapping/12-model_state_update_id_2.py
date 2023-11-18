66c884a8992c31ad4752a76310b309deafc784ff
Okeomasilachi <okeomasilachi@gmail.com>
2023-11-18 18:43:34 +0100

    Write a script that changes the name of a State object from the database hbtn_0e_6_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    Change the name of the State where id = 2 to New Mexico
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+"""prints the first State object from the database hbtn_0e_6_usa"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    data = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(data, pool_pre_ping=True)
+    Base.metadata.bind = engine
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    state = ses.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    ses.commit()
