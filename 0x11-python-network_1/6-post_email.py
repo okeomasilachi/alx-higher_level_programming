6246f0197ffd6a35075d292fe576cf6a5e7bdf70
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-03 14:13:10 +0100

    Write a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
    The email must be sent in the variable email
    You must use the packages requests and sys
    You are not allowed to import packages other than requests and sys
    You don’t need to error check arguments passed to the script (number or type)

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""sends a POST request to the
+passed URL with the email as a parameter"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.post(argv[1], data={"email": argv[2]})
+    value = r.content.decode("UTF-8")
+    print(value)
