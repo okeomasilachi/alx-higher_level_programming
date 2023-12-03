13831325db1583b34093fedbc16f6e9229e7c74b
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-03 14:46:32 +0100

    Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
    The letter must be sent in the variable q
    If no argument is given, set q=""
    If the response body is properly JSON formatted and not empty, display the id and name like this: [<id>] <name>
    Otherwise:
    Display Not a valid JSON if the JSON is invalid
    Display No result if the JSON is empty
    You must use the package requests and sys
    You are not allowed to import packages other than requests and sys

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+"""
+sends a POST request to
+http://0.0.0.0:5000/search_user
+with the letter as a parameter."""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) <= 1:
+        p = {'q': ""}
+    else:
+        p = {'q': sys.argv[1]}
+    r = requests.post(url, data=p)
+    try:
+        j = r.json()
+        if j:
+            print(f"[{j['id']}] {j['name']}")
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
