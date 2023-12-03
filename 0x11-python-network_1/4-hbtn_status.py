88b759c84d1033bb4effed5f086e6ef6b357170e
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-03 13:52:38 +0100

    Write a Python script that fetches https://alx-intranet.hbtn.io/status
    You must use the package requests
    You are not allow to import packages other than requests
    The body of the response must be display like the following example (tabulation before -)

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    data = r.content.decode("UTF-8")
+    print("Body response:")
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
