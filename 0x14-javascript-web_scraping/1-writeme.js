69469f738425f5f42a6514606a5b6bb4cda24a61
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-04 00:04:42 +0100

    Write a script that writes a string to a file.
    The first argument is the file path
    The second argument is the string to write
    The content of the file must be written in utf-8
    If an error occurred during while writing, print the error object

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node\n
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  process.exit();
+}
+
+const path = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(path, string, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+  }
+});
