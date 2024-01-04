2e3d908937d929a3ada99796cfc9c1f29a01a345
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-04 01:11:08 +0100

    Write a script that gets the contents of a webpage and stores it in a file.
    The first argument is the URL to request
    The second argument the file path to store the body response
    The file must be UTF-8 encoded
    You must use the module request

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node\n
+#!/usr/bin/node
+
+const rq = require('request');
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  process.exit();
+}
+
+// Request URL
+const url = process.argv[2];
+const path = process.argv[3];
+
+rq(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  }
+
+  const value = response.body;
+  fs.writeFile(path, value, 'utf-8', (error, data) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+});
