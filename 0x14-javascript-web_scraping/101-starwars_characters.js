68e77fdb282822910519dff46b46ca5fa541889a
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-04 02:47:15 +0100

    Write a script that prints all characters of a Star Wars movie:
    The first argument is the Movie ID - example: 3 = “Return of the Jedi”
    Display one character name by line in the same order of the list “characters” in the /films/ response
    You must use the Star wars API
    You must use the module request

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,56 @@
 #!/usr/bin/node
 
-const rq = require('request');
+const request = require('request');
 
 if (process.argv.length < 3) {
-  process.exit();
+  console.error('Usage: node movie_characters.js <Movie_ID>');
+  process.exit(1);
 }
 
-// Request URL
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-rq(url, (error, response) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
+    process.exit(1);
   }
 
-  let value = response.toJSON();
-  value = JSON.parse(value.body);
-  for (let i = 0; i < value.characters.length; i++) {
-    rq(value.characters[i], (error, res) => {
-      if (error) {
-        console.error(error);
-      }
-
-      let value = res.toJSON();
-      value = JSON.parse(value.body);
-      console.log(value.name);
-    });
+  if (response.statusCode !== 200) {
+    console.error(`Unexpected response: ${response.statusCode}`);
+    process.exit(1);
   }
+
+  const movieData = JSON.parse(body);
+  const characterUrls = movieData.characters;
+
+  if (characterUrls.length === 0) {
+    console.log(`No characters found for Movie ID ${movieId}`);
+    process.exit(0);
+  }
+
+  // Use async/await to ensure characters are printed in order
+  (async () => {
+    for (const characterUrl of characterUrls) {
+      const characterData = await fetchCharacterData(characterUrl);
+      console.log(characterData.name);
+    }
+  })();
 });
+
+function fetchCharacterData (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        // Use an Error object for better error handling
+        reject(new Error(charError));
+      } else if (charResponse.statusCode !== 200) {
+        // Use an Error object for better error handling
+        reject(new Error(`Unexpected response for character: ${charResponse.statusCode}`));
+      } else {
+        const characterData = JSON.parse(charBody);
+        resolve(characterData);
+      }
+    });
+  });
+}
