c050b1f7e69bc0b35880fd2b94b0b001ef7245b8
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-11 05:30:35 +0100

    Add AJAX request to fetch Star Wars movies and display them in a list

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,12 @@
+$(() => {
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    success: (data) => {
+      console.log(data.results);
+      $.each(data.results, (indexInArray, valueOfElement) => {
+        $('ul#list_movies').append('<li>' + valueOfElement.title + '</li>');
+      });
+    }
+  });
+});
