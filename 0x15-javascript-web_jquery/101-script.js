6865c2e604263c1ad7a697d8472e12ffca344f51
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-11 05:49:18 +0100

    Add jQuery functionality to manipulate list items

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+  $('div#add_item').click(() => {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+
+  $('div#remove_item').click(() => {
+    $('ul.my_list li:last-child').remove();
+  });
+
+  $('div#clear_list').click(() => {
+    $('ul.my_list').empty();
+  });
+});
